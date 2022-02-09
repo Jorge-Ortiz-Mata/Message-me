4cae5c52c447675ddcedac0c9a84286b7aea0573
Jorge-Ortiz <ortiz.mata.jorge@gmail.com>
2022-02-09 12:54:36 -0600

    Chat and auto scroll working.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+$('document').ready(function() {
+    $('#messages').animate({scrollTop: 1000000}, 800);
+});
+
+document.addEventListener("turbo:before-stream-render", function(e){
+    $('document').ready(function() {
+        $('#messages').animate({scrollTop: 1000000}, 800);
+    });
+});
